a3f440ec9f4a4b7a8a9746b75e57d16f0b58e301
IS729303 <is729303@iteso.mx>
2024-02-20 07:18:43 -0600

    Plantilla

---
+++ crypto/lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Crypto APP"),
+      ),
+      body: Container(),
+    );
+  }
+}
